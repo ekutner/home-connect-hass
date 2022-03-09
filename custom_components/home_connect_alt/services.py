bc57f5ed715343bf068cf530de6f1a2ad20874ba
Yuval Aboulafia <yuval.abou@gmail.com>
2022-03-09 21:51:17 +0200

    Format services

--- custom_components/home_connect_alt/services.py
+++ custom_components/home_connect_alt/services.py
@@ -5,54 +5,59 @@ from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import device_registry as dr
 
 
-class Services():
-    """ Collection of the Services offered by the integration """
-    def __init__(self, hass:HomeAssistant,  homeconnect:HomeConnect) -> None:
+class Services:
+    """Collection of the Services offered by the integration."""
+
+    def __init__(self, hass: HomeAssistant, homeconnect: HomeConnect) -> None:
         self.homeconnect = homeconnect
         self.hass = hass
         self.dr = dr.async_get(hass)
 
     async def async_select_program(self, call) -> None:
-        """ Service for selecting a program """
+        """Service for selecting a program."""
         data = call.data
-        appliance = self.get_appliance_from_device_id(data['device_id'])
+        appliance = self.get_appliance_from_device_id(data["device_id"])
         if appliance:
-            program_key = data['program_key']
-            options = data.get('options')
+            program_key = data["program_key"]
+            options = data.get("options")
             try:
                 await appliance.async_select_program(program_key, options)
             except HomeConnectError as ex:
-                raise HomeAssistantError(ex.error_description if ex.error_description else ex.msg)
+                raise HomeAssistantError(
+                    ex.error_description if ex.error_description else ex.msg
+                )
 
     async def async_start_program(self, call) -> None:
-        """ Service for starting the currently selected program """
+        """Service for starting the currently selected program."""
         data = call.data
-        appliance = self.get_appliance_from_device_id(data['device_id'])
+        appliance = self.get_appliance_from_device_id(data["device_id"])
         if appliance:
-            program_key = data.get('program_key')
-            options = data.get('options')
+            program_key = data.get("program_key")
+            options = data.get("options")
             try:
                 await appliance.async_start_program(program_key, options)
             except HomeConnectError as ex:
-                raise HomeAssistantError(ex.error_description if ex.error_description else ex.msg)
-
+                raise HomeAssistantError(
+                    ex.error_description if ex.error_description else ex.msg
+                )
 
     async def async_stop_program(self, call) -> None:
-        """ Service for stopping the currently active program """
+        """Service for stopping the currently active program."""
         data = call.data
-        appliance = self.get_appliance_from_device_id(data['device_id'])
+        appliance = self.get_appliance_from_device_id(data["device_id"])
         if appliance:
             try:
                 await appliance.async_stop_active_program()
             except HomeConnectError as ex:
-                raise HomeAssistantError(ex.error_description if ex.error_description else ex.msg)
-
+                raise HomeAssistantError(
+                    ex.error_description if ex.error_description else ex.msg
+                )
 
     def get_appliance_from_device_id(self, device_id):
-        """ Helper function to get an appliance from the Home Assistant device_id """
+        """Helper function to get an appliance from the Home Assistant device_id."""
         device = self.dr.devices[device_id]
         haId = list(device.identifiers)[0][1]
         for (key, appliance) in self.homeconnect.appliances.items():
-            if key.lower().replace('-','_') == haId:
+            if key.lower().replace("-", "_") == haId:
                 return appliance
         return None
