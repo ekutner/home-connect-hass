a0f43189f74ff98226918bfe7b7c7d963786c450
ekutner <>
2022-06-03 23:41:15 +0300

    Fix typo in sensor_value_translation param

--- custom_components/home_connect_alt/const.py
+++ custom_components/home_connect_alt/const.py
@@ -10,7 +10,7 @@ SCOPES = "IdentifyAppliance Monitor Control Settings"
 CONF_SIMULATE = "simulate"
 CONF_LANG = "language"
 CONF_CACHE = "cache"
-CONF_SENSORS_TRANSLATION = "sensors_value_translation"
+CONF_SENSORS_TRANSLATION = "sensor_value_translation"
 
 HOME_CONNECT_DEVICE = {
     "identifiers": {(DOMAIN, "homeconnect")},
