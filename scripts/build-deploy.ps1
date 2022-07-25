cf6b75ca3cd4c4fcc88139c3360c6b763c21314c
ekutner <>
2022-07-26 00:42:28 +0300

    Refactor cover buttons on gallery single page

--- scripts/build-deploy.ps1
+++ scripts/build-deploy.ps1
@@ -8,6 +8,6 @@ $mypath = Split-Path -path $MyInvocation.MyCommand.Path
 
 
 #& "$mypath\hugo.exe" -v --templateMetrics --templateMetricsHints --cleanDestinationDir
-& "$mypath\hugo.exe" -v --templateMetrics --templateMetricsHints 
+& "$mypath\hugo.exe" -v --templateMetrics --templateMetricsHints --minify
 & "$mypath\hugo.exe" deploy -v --maxDeletes -1
 #aws s3 sync public/ "s3://photos.kutner.org" --delete
